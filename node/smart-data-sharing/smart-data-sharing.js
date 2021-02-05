e9b87ab8e0b08f21bcba5e7147be55bba80910d6
muneeb706 <muneeb706@gmail.com>
2021-02-04 23:10:58 -0600

    smart data sharing (3) (final)

--- node/smart-data-sharing/smart-data-sharing.js
+++ node/smart-data-sharing/smart-data-sharing.js
@@ -1,16 +1,3 @@
-// Muaz Khan   - www.MuazKhan.com
-// MIT License - www.WebRTC-Experiment.com/licence
-
-// WebRTC Scalable Broadcast:
-// this module simply initializes socket.io
-// and configures it in a way that
-// single broadcast can be relayed over unlimited users
-// without any bandwidth/CPU usage issues.
-// Everything happens peer-to-peer!
-
-// Ref. discussion: https://github.com/muaz-khan/WebRTC-Experiment/issues/2
-// Source Code: https://github.com/muaz-khan/WebRTC-Scalable-Broadcast
-
 module.exports = exports = Smart_Data_Sharing;
 
 function Smart_Data_Sharing(app, file_data, server_id) {
@@ -22,8 +9,8 @@ function Smart_Data_Sharing(app, file_data, server_id) {
 
     io.set('transports', [
         'websocket', // 'disconnect' EVENT will work only with 'websocket'
-        'xhr-polling',
-        'jsonp-polling'
+        // 'xhr-polling',
+        // 'jsonp-polling'
     ]);
 
     // // this RTCMultiConnection object is used to connect with existing users
@@ -48,18 +35,10 @@ function Smart_Data_Sharing(app, file_data, server_id) {
             }
             let data = file_data.split("\n")
             let firstChunk = data[0]
-            var broadcaster = getBroadcaster(user);
-            if (broadcaster) {
-                listOfBroadcasts[user.broadcastid].broadcaster.numberOfViewers++;
-                socket.emit('join-broadcaster', broadcaster, listOfBroadcasts[user.broadcastid].typeOfStreams, firstChunk);
-                console.log('User <', user.userid, '> is trying to get stream from user <', broadcaster.userid, '>');
+            
 
-            } else {
-                currentUser.isInitiator = true;
-                listOfBroadcasts[user.broadcastid].broadcaster = user;
-                socket.emit('start-broadcasting', listOfBroadcasts[user.broadcastid].typeOfStreams, firstChunk);
-                console.log('User <', user.userid, '> will serve broadcast ( ', user.broadcastid, ' ).');
-            }
+            socket.emit('start-broadcasting', listOfBroadcasts[user.broadcastid].typeOfStreams, firstChunk);
+            
             // socket.emit('start-broadcasting', listOfBroadcasts[user.broadcastid].typeOfStreams);
 
             console.log('User <', user.userid, '> has joined the broadcast ( ', user.broadcastid, ' ).');
@@ -80,9 +59,9 @@ function Smart_Data_Sharing(app, file_data, server_id) {
         });
 
 
-        socket.on('message', function(message) {
-            socket.broadcast.emit('message', message);
-        });
+        // socket.on('message', function(message) {
+        //     socket.broadcast.emit('message', message);
+        // });
 
         socket.on('disconnect', function() {
             // needs to be implemented
@@ -96,17 +75,6 @@ function Smart_Data_Sharing(app, file_data, server_id) {
             //     delete listOfBroadcasts[currentUser.broadcastid];
             // }
         });
-
-        function getBroadcaster(user) {
-            var broadcaster = listOfBroadcasts[user.broadcastid].broadcaster;
-            // for (var userid in broadcasters) {
-            //     if (broadcasters[userid].numberOfViewers <= 3) {
-            //         firstResult = broadcasters[userid];
-            //         continue;
-            //     } else delete listOfBroadcasts[user.broadcastid].broadcasters[userid];
-            // }
-            return broadcaster;
-        }
         
     })
 
