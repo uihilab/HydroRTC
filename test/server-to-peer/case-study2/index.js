12e31777e849de5a5b85fec3a64ef3f7854d3124
erazocar <erazocar@users.noreply.github.com>
2024-09-04 18:58:16 +0000

    deploy: 3ed68cc14af37b8b56ccda3a787123e1e3dcead1

--- test/server-to-peer/case-study2/index.js
+++
@@ -1,8 +0,0 @@
-// importing server from library
-
-const HydroRTC = require('./node_modules/hydro-rtc/hydrortcserver.js')
-
-const hydroRtcServer = new HydroRTC('my-server')
-hydroRtcServer.run('localhost', 2000)
-// configuring tasks, which will be distributed by servers
-hydroRtcServer.setTasks(['smart-data-transmission'])
